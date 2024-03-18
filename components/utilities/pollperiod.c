c62704480b5e51a6ba3920dfc0dba432066d0b68
Simeon Tran <siatran@ucsc.edu>
2024-03-18 01:11:41 -0700

    macro to get openthread instance

--- components/utilities/pollperiod.c
+++ components/utilities/pollperiod.c
@@ -1,18 +1,18 @@
 #include "utilities.h"
 
+#define OT_INSTANCE esp_openthread_get_instance()
+
 void setPollPeriod(uint32_t pollPeriod) {
-  otInstance *aInstance = esp_openthread_get_instance();
+  otLogNotePlat("Poll period is currently at %" PRIu32 ".",
+                otLinkGetPollPeriod(OT_INSTANCE));
 
-  otLogNotePlat("Poll period is currently %" PRIu32 "",
-                otLinkGetPollPeriod(aInstance));
-
-  otError error = otLinkSetPollPeriod(aInstance, pollPeriod);
+  otError error = otLinkSetPollPeriod(OT_INSTANCE, pollPeriod);
   if (error == OT_ERROR_INVALID_ARGS) {
-    otLogCritPlat("Failed to set poll period with value %" PRIu32 "", pollPeriod);
+    otLogCritPlat("Failed to set poll period with value %" PRIu32 ".", pollPeriod);
     return;
   }
 
-  uint32_t newPollPeriod = otLinkGetPollPeriod(aInstance);
+  uint32_t newPollPeriod = otLinkGetPollPeriod(OT_INSTANCE);
   assert(newPollPeriod == pollPeriod);
   otLogNotePlat("Poll period has now been set to %" PRIu32 "", newPollPeriod);
 
